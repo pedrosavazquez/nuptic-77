c43481a9e4713f8b99a54e06a00b5e7324ae6427
codigomorrazo <ipedrosacip@gmail.com>
2022-10-24 08:25:15 +0200

    Used logger to store East direction info

--- application/src/Infrastructure/Middleware/DirectionRegister.php
+++ application/src/Infrastructure/Middleware/DirectionRegister.php
@@ -4,19 +4,89 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Middleware;
 
-use App\Application\Nuptic\Command\RegisterNupticCommand;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\ReceivedStamp;
+use Symfony\Component\Messenger\Stamp\SentStamp;
+use Throwable;
 
-final class DirectionRegister
+final class DirectionRegister implements MiddlewareInterface
 {
-    private const DIRECTION_REQUEST_LOG_PATH='directionRequestLogPath';
     private const EAST = 'East';
 
-    public function __invoke(RegisterNupticCommand $command): void
+    public function __construct(private readonly LoggerInterface $busLogger)
+    {}
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
-        if (self::EAST !== $command->direction) {
+        $message = $envelope->getMessage();
+
+        if (self::EAST !== $message->direction) {
+            return $stack->next()->handle($envelope, $stack);
+        }
+
+        $context = [
+            'message' => $this->getClassName($message),
+            'content' => (string)$message,
+            'class' => $message::class,
+        ];
+
+        [$envelope, $exception] = $this->passMessageToNextHandler($envelope, $stack);
+
+        $messageTemplate = $this->prepareMessageTemplate($envelope, $exception);
+
+        $this->logMessageAndPassException($messageTemplate, $context, $exception);
+
+        return $envelope;
+    }
+
+    private function getClassName($message): string
+    {
+        $fullName = $message::class;
+
+        $parts = explode('\\', $fullName);
+
+        return end($parts);
+    }
+
+    private function passMessageToNextHandler(Envelope $envelope, StackInterface $stack): array
+    {
+        $exception = null;
+        try {
+            $envelope = $stack->next()
+                            ->handle($envelope, $stack);
+        } catch (Throwable $exceptionHandlingMessage) {
+            $exception = $exceptionHandlingMessage;
+        }
+
+        return [$envelope, $exception];
+    }
+
+    private function prepareMessageTemplate(Envelope $envelope, ?Throwable $exception): string
+    {
+        if ($envelope->last(ReceivedStamp::class)) {
+            $logMessage = 'Received';
+        } elseif ($envelope->last(SentStamp::class)) {
+            $logMessage = 'Sent';
+        } else {
+            $logMessage = 'Handling';
+        }
+
+        if (null === $exception) {
+            $logMessage .= ' {message} {content}';
+        }
+
+        return $logMessage;
+    }
+
+    private function logMessageAndPassException(string $logMessage, array $context, ?Throwable $exception): void
+    {
+        if ($exception) {
             return;
         }
-        $path = getenv(self::DIRECTION_REQUEST_LOG_PATH);
-        file_put_contents($path, json_encode($command, JSON_THROW_ON_ERROR, 512));
+
+        $this->busLogger->info($logMessage, $context);
     }
 }
